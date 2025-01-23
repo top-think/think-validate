8fdfa909064fb392c3ac0aabf5f56e6f353e0d65
thinkphp <thinkphp@qq.com>
2025-01-23 10:54:10 +0800

    独立使用的时候默认支持中文提示信息

--- src/Validate.php
+++ src/Validate.php
@@ -145,6 +145,69 @@ class Validate
         'multipleOf'  => ':attribute must multiple :rule',
     ];
 
+    protected $typeMsgZh = [
+        'require'     => ':attribute不能为空',
+        'must'        => ':attribute必须',
+        'number'      => ':attribute必须是数字',
+        'integer'     => ':attribute必须是整数',
+        'float'       => ':attribute必须是浮点数',
+        'string'      => ':attribute必须是字符串',
+        'enum'        => ':attribute必须是有效的 :rule 枚举',
+        'startWith'   => ':attribute必须以 :rule 开头',
+        'endWith'     => ':attribute必须以 :rule 结尾',
+        'contain'     => ':attribute必须包含 :rule',
+        'boolean'     => ':attribute必须是布尔值',
+        'email'       => ':attribute格式不符',
+        'mobile'      => ':attribute格式不符',
+        'array'       => ':attribute必须是数组',
+        'accepted'    => ':attribute必须是yes、on、true或者1',
+        'acceptedIf'  => ':attribute必须是yes、on、true或者1',
+        'declined'    => ':attribute必须是no、off、false或者0',
+        'declinedIf'  => ':attribute必须是no、off、false或者0',
+        'date'        => ':attribute不是一个有效的日期或时间格式',
+        'file'        => ':attribute不是有效的上传文件',
+        'image'       => ':attribute不是有效的图像文件',
+        'alpha'       => ':attribute只能是字母',
+        'alphaNum'    => ':attribute只能是字母和数字',
+        'alphaDash'   => ':attribute只能是字母、数字和下划线_及破折号-',
+        'activeUrl'   => ':attribute不是有效的域名或者IP',
+        'chs'         => ':attribute只能是汉字',
+        'chsAlpha'    => ':attribute只能是汉字、字母',
+        'chsAlphaNum' => ':attribute只能是汉字、字母和数字',
+        'chsDash'     => ':attribute只能是汉字、字母、数字和下划线_及破折号-',
+        'url'         => ':attribute不是有效的URL地址',
+        'ip'          => ':attribute不是有效的IP地址',
+        'dateFormat'  => ':attribute必须使用日期格式 :rule',
+        'in'          => ':attribute必须在 :rule 范围内',
+        'notIn'       => ':attribute不能在 :rule 范围内',
+        'between'     => ':attribute只能在 :1 - :2 之间',
+        'notBetween'  => ':attribute不能在 :1 - :2 之间',
+        'length'      => ':attribute长度不符合要求 :rule',
+        'max'         => ':attribute长度不能超过 :rule',
+        'min'         => ':attribute长度不能小于 :rule',
+        'after'       => ':attribute日期不能小于 :rule',
+        'before'      => ':attribute日期不能超过 :rule',
+        'expire'      => '不在有效期内 :rule',
+        'allowIp'     => '不允许的IP访问',
+        'denyIp'      => '禁止的IP访问',
+        'confirm'     => ':attribute和确认字段:2不一致',
+        'different'   => ':attribute和比较字段:2不能相同',
+        'egt'         => ':attribute必须大于等于 :rule',
+        'gt'          => ':attribute必须大于 :rule',
+        'elt'         => ':attribute必须小于等于 :rule',
+        'lt'          => ':attribute必须小于 :rule',
+        'eq'          => ':attribute必须等于 :rule',
+        'neq'         => ':attribute不能等于 :rule',
+        'unique'      => ':attribute已存在',
+        'regex'       => ':attribute不符合指定规则',
+        'multipleOf'  => ':attribute必须是 :rule 的倍数',
+        'fileSize'    => '文件大小不符',
+        'fileExt'     => '文件后缀不允许',
+        'fileMime'    => '文件类型不允许',        
+        'method'      => '无效的请求类型',
+        'token'       => '令牌数据无效',
+    ];
+
     /**
      * 当前验证场景
      * @var string
@@ -2019,6 +2082,8 @@ class Validate
             $msg = $this->message[$attribute];
         } elseif (isset($this->typeMsg[$type])) {
             $msg = $this->typeMsg[$type];
+        } elseif (!$this->lang && isset($this->typeMsgZh[$type])) {
+            $msg = $this->typeMsgZh[$type];
         } elseif (str_starts_with($type, 'require')) {
             $msg = $this->typeMsg['require'];
         } else {
